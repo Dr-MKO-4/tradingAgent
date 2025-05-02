be5d1a59dc4d26be6d7d0e25817b62a5b61a612c
frankyankam <149747993+frankyankam@users.noreply.github.com>
2025-05-02 06:31:31 +0100

    Delete frontEnd directory

--- frontEnd/src/features/History/historyAPI.js
+++
@@ -1,7 +0,0 @@
-// src/features/History/historyAPI.js
-export async function fetchHistory() {
-    const response = await fetch('/api/history');
-    const data = await response.json();
-    return data;
-  }
-  
--- frontEnd/src/utils/formatDate.js
+++
@@ -1,8 +0,0 @@
-// Chemin : src/utils/formatDate.js
-export function formatDate(timestamp) {
-  const date = new Date(timestamp);
-  return date.toLocaleString('fr-FR', {
-    year: 'numeric', month: '2-digit', day: '2-digit',
-    hour: '2-digit', minute: '2-digit', second: '2-digit'
-  });
-}
